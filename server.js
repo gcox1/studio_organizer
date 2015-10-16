133279522bb777e31d30151bf0059decce96105e
Gregory Cox <gregorywcox@gmail.com>
2015-10-16 18:06:40 +0000

    implementing templating

--- server.js
+++ server.js
@@ -5,19 +5,36 @@ var path = require('path');
 var bodyParser = require('body-parser')
 
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(bodyParser());
 app.get('/', function (req, res){
-  console.log('Loading home page')
-  res.sendFile(path.join(__dirname + '/lessonPage.html'));
+  //call function to get list
+  ls = [['Lesson with Patrick', '10/16/2015'], ['Lesson with Greg', '10/15/2015']];
+
+  res.render('index', {lessons:ls});
+//  console.log('Loading home page')
+//  res.sendFile(path.join(__dirname + '/lessonPage.html'));
 });
 
 app.post('/add_lesson', function(req,res){
-  console.log('Adding as lesson');
+  console.log('Adding a lesson');
   var leson_name = req.body.lesson_name;
   var date = req.body.date;
   //call sql add function
   res.send('Lesson Added');
   //res.sendFile(path.join(__dirname + '/lessonPage.html'));
 
+});
+
+app.post('/verify_lesson', function(req, res){
+  console.log('Verifying lesson');
+  var is_verified = req.body.is_verified; //'on' or undefined
+  var
+  if (is_verified == 'on'){
+    console.log(is_verified);
+  }
+  res.send('Lesson Verified');
 });
 
 var server = app.listen(8888,function () {
