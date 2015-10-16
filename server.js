d66b41087d094a7e3786493414e7818e9c54d7c6
Gregory Cox <gregorywcox@gmail.com>
2015-10-16 05:49:38 +0000

    page outline pre templating

--- server.js
+++ server.js
@@ -2,11 +2,23 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 var path = require('path');
+var bodyParser = require('body-parser')
+
 
 app.get('/', function (req, res){
+  console.log('Loading home page')
   res.sendFile(path.join(__dirname + '/lessonPage.html'));
 });
 
+app.post('/add_lesson', function(req,res){
+  console.log('Adding as lesson');
+  var leson_name = req.body.lesson_name;
+  var date = req.body.date;
+  //call sql add function
+  res.send('Lesson Added');
+  //res.sendFile(path.join(__dirname + '/lessonPage.html'));
+
+});
 
 var server = app.listen(8888,function () {
   var host = server.address().address;
